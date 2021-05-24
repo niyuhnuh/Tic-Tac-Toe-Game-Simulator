c07a771e7668f56893cd07510a99782501c1ef30
niyuhnuh <nianiaandrea@gmail.com>
2021-05-24 17:49:37 +0800

    New form for the actual game

---
+++ Tic-Tac-Toe Game Simulator/Form2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe_Game_Simulator
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+    }
+}
