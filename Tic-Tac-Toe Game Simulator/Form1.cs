52ad46606d774cee7aa162960aef05c490c4b93f
niyuhnuh <nianiaandrea@gmail.com>
2021-05-29 13:57:47 +0800

    Assigning horizontal wins (4)

--- Tic-Tac-Toe Game Simulator/Form1.cs
+++ Tic-Tac-Toe Game Simulator/Form1.cs
@@ -24,7 +24,7 @@ namespace Tic_Tac_Toe_Game_Simulator
         {
             //Horizontal Wins 
 
-           if ((btn1.Text == btn2.Text) && (btn2.Text == btn3.Text) && (countingTurns >= 5))
+           if ((btn1.Text == btn2.Text) && (btn2.Text == btn3.Text) && (btn1.Text == btn3.Text) && (countingTurns >= 5))
            {
                 lbl_Status.Visible = true;
                 lbl_Status.Text = "Game Over!";
@@ -32,7 +32,7 @@ namespace Tic_Tac_Toe_Game_Simulator
                 lbl_Turn.Text = player + " is a Winner";
            }
 
-            if ((btn4.Text == btn5.Text) && (btn5.Text == btn6.Text) && (countingTurns >= 5))
+            if ((btn4.Text == btn5.Text) && (btn5.Text == btn6.Text) && (btn6.Text == btn4.Text) && (countingTurns >= 5))
             {
                 lbl_Status.Visible = true;
                 lbl_Status.Text = "Game Over!";
@@ -40,7 +40,7 @@ namespace Tic_Tac_Toe_Game_Simulator
                 lbl_Turn.Text = player + " is a Winner";
             }
 
-            if ((btn7.Text == btn8.Text) && (btn8.Text == btn9.Text) && (countingTurns >= 5))
+            if ((btn7.Text == btn8.Text) && (btn8.Text == btn9.Text) && (btn9.Text == btn7.Text) && (countingTurns >= 5))
             {
                 lbl_Status.Visible = true;
                 lbl_Status.Text = "Game Over!";
