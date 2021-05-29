55fe9e4d497fa3a312b18628b1cd4c154f8e7a28
niyuhnuh <nianiaandrea@gmail.com>
2021-05-29 17:09:29 +0800

    Conditions for a draw (1)

--- Tic-Tac-Toe Game Simulator/Form1.cs
+++ Tic-Tac-Toe Game Simulator/Form1.cs
@@ -226,21 +226,11 @@ namespace Tic_Tac_Toe_Game_Simulator
                
             }
 
-            else if (countingTurns >= 9)
+            else if (!btn1.Enabled && !btn2.Enabled && !btn3.Enabled && !btn4.Enabled && !btn5.Enabled && !btn6.Enabled && !btn7.Enabled && !btn8.Enabled && !btn9.Enabled)
             {
                 lbl_Status.Visible = true;
                 lbl_Status.Text = "Its a Tie!";
                 lbl_Turn.Text = "Its a Tie!";
-
-                btn1.Enabled = false;
-                btn2.Enabled = false;
-                btn3.Enabled = false;
-                btn4.Enabled = false;
-                btn5.Enabled = false;
-                btn6.Enabled = false;
-                btn7.Enabled = false;
-                btn8.Enabled = false;
-                btn9.Enabled = false;
 
             }
 
